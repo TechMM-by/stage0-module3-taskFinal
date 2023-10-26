0acaa6546dfcb4958be7c4f2dffb7aadf765171a
Maria Maksimova <drogzed@gmail.com>
2023-10-26 22:07:44 +0400

    first

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,14 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+
+        int first =  number / 1000;
+        int two = (number/ 100) % 10;
+
+        int three = (number / 10) % 10;
+        int four = number % 10;
+
+        System.out.println(first + two + three + four);
+
     }
 }
